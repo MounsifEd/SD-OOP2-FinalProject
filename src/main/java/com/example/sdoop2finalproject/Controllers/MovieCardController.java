4aab460f7ad70842736f95ac0de47cc0c11f4a45
Mounsif <wdadsa@fsdfdsa.com>
2025-11-23 10:12:26 -0500

    Add Initial Setup for the feature.

---
+++ src/main/java/com/example/sdoop2finalproject/Controllers/MovieCardController.java
@@ -0,0 +1,18 @@
+package com.example.sdoop2finalproject.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class MovieCardController {
+    @FXML
+    private ImageView posterView;
+    @FXML
+    private Text movieTitle;
+    @FXML
+    private Text movieGenre;
+    @FXML
+    private Text movieRelease;
+
+    private Movie movie;
+}
