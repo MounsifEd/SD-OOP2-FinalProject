513cb50d99955fe4607da4a97b55077ba4d10ab7
Mounsif <wdadsa@fsdfdsa.com>
2025-11-24 20:59:47 -0500

    Add alert in ShowtimeCardController when clicking on select button.

--- src/main/java/com/example/sdoop2finalproject/Controllers/ShowtimeCardController.java
+++ src/main/java/com/example/sdoop2finalproject/Controllers/ShowtimeCardController.java
@@ -3,6 +3,8 @@ package com.example.sdoop2finalproject.Controllers;
 import com.example.sdoop2finalproject.Models.MovieShow;
 import javafx.fxml.FXML;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
 /**
@@ -29,6 +31,9 @@ public class ShowtimeCardController {
     @FXML
     private Text roomText;
 
+    @FXML
+    private Button selectBtn;
+
 
     /**
      * Change UI components.
@@ -38,6 +43,20 @@ public class ShowtimeCardController {
         dateText.setText(pShow.getShowDate());
         timeText.setText(pShow.getAshowTime());
         roomText.setText("Room " + pShow.getRoomID());
+
+        selectBtn.setOnAction(e -> showUnavailablePaymentAlert());
     }
+
+    /**
+     * Shows an alert telling the user that payment is unavailable.
+     */
+    private void showUnavailablePaymentAlert() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Ticket Payment");
+        alert.setHeaderText("Payment Unavailable");
+        alert.setContentText("The payment method is currently unavailable. Please try later. ");
+        alert.showAndWait();
+    }
+
 }
 
